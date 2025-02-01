8a53a5adc0e2806886945f24ebfb6521b2619899
Piyush Kumar <piyushkumar1116@gmail.com>
2025-02-01 22:39:25 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,8 @@
 import React from "react";
-// import { BookOpen, Users, Trophy } from "lucide-react"; 
-import HeroCarousel from "@/components/home/components/hero";
-import SchoolHomepage from "@/components/home/components/other";
+import HomePage from "@/components/home";
 
-const HeroSection = () => {
-  // const stats = [
-  //   {
-  //     icon: <BookOpen className="h-6 w-6" />,
-  //     value: "3+",
-  //     label: "Years of Excellence",
-  //   },
-  //   {
-  //     icon: <Users className="h-6 w-6" />,
-  //     value: "1000+",
-  //     label: "Happy Students",
-  //   },
-  //   {
-  //     icon: <Trophy className="h-6 w-6" />,
-  //     value: "100%",
-  //     label: "Success Rate",
-  //   },
-  // ];
-
-  return (
-    <>
-      <div className="flex flex-col justify-center max-w-screen-xl mx-auto  py-4 font-bold">
-        <h1>MOTION EDUCARE SCHOOL</h1>
-      </div>
-      <HeroCarousel />
-
-      {/* Content */}
-      <SchoolHomepage />
-    </>
-  );
+const Home = () => {
+  return <HomePage />;
 };
 
-export default HeroSection;
+export default Home;
---
+++ src/components/home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroCarousel from "./components/hero";
+import SchoolHomepage from "./components/other";
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroCarousel />
+      <SchoolHomepage />
+    </>
+  );
+};
+
+export default HomePage;
